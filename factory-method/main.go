4e218b659d7d13d02e7b633ef7ce714fa004687e
ManojChandran <manoj.meparambu@gmail.com>
2025-03-27 19:02:18 +0530

    update factory method

---
+++ factory-method/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+// Defines an interface for creating objects but allows subclasses (factories) to decide which class to instantiate.
+// Step 1: Define the Notification interface
+type Notification interface {
+	SendNotification()
+}
+
+// Step 2: Concrete implementations
+type EmailNotification struct{}
+
+func (e *EmailNotification) SendNotification() {
+	fmt.Println("Send email notification")
+}
+
+type SMSNotification struct{}
+
+func (s *SMSNotification) SendNotification() {
+	fmt.Println("Send SMS notofocation")
+}
+
+// Step 3: Define Factory interface
+type NotificationFactory interface {
+	CreateNotification() Notification
+}
+
+// Step 4: Implement concrete factories
+type EmailFactory struct{}
+
+func (e *EmailFactory) CreateNotification() Notification {
+	return &EmailNotification{}
+}
+
+type SMSFactory struct{}
+
+func (s *SMSFactory) CreateNotification() Notification {
+	return &SMSNotification{}
+}
+
+// Step 5: Usage
+func main() {
+	emailfactory := &EmailFactory{}
+	email := emailfactory.CreateNotification()
+	email.SendNotification()
+
+	smsFactory := &SMSFactory{}
+	sms := smsFactory.CreateNotification()
+	sms.SendNotification()
+}
