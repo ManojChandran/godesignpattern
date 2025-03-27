1bcc18e09429cb75b7166f3f646a14faa362684f
ManojChandran <manoj.meparambu@gmail.com>
2025-03-27 16:17:41 +0530

    added simple factory

---
+++ simple-factory/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Step 1: Define an interface
+type Notification interface {
+	SendNotification()
+}
+
+// Step 2: Implement concrete classes
+type EmailNotification struct{}
+
+func (*EmailNotification) SendNotification() {
+	fmt.Println("Send Email notification")
+}
+
+type SMSNotification struct{}
+
+func (*SMSNotification) SendNotification() {
+	fmt.Println("Send SMS notification")
+}
+
+// Step 3: Create a Simple Factory Function
+func NotificationFactory(notificationType string) Notification {
+	switch notificationType {
+	case "email":
+		return &EmailNotification{}
+	case "sms":
+		return &SMSNotification{}
+	default:
+		return nil
+	}
+}
+
+// Step 4: Usage
+func main() {
+	email := NotificationFactory("email")
+	email.SendNotification()
+
+	sms := NotificationFactory("sms")
+	sms.SendNotification()
+}
